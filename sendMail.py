343119fd05f404bd14077308c0e8c640a1177efe
Pap Gergely <91294375+yrogerg0@users.noreply.github.com>
2021-10-02 17:46:50 +0200

    update sendMail.py
    + added OSError exception;

--- sendMail.py
+++ sendMail.py
@@ -5,126 +5,129 @@ from time import sleep
 import platform
 import sys
 
-# függvények, parancsikonok (shortcuts)
+# detects what platform you open the app from
+# pre-defines the clear function based on your os.
 if platform.system() == "Windows":
-    # pre-defining the clear function
     def clear():
         system("cls")
 else:
-    # pre-defining the clear function
     def clear():
         system("clear")
 
 def en():
     clear()
-    def idiota(var):
+    # it's self-explanatory
+    def idiot(var):
         if var != "":
             print("PRESSING ENTER WOULD'VE BEEN ENOUGH, BUT OK!")
             sleep(0.69420)
         clear()
-    # megjegyzések a felhasználó számára
+    # outputs a disclaimer for the user
     print("DISCLAIMER: READ THE README.MD DOCUMENT BEFORE USING THIS PROGRAM!")
     print("ALSO, THIS PROGRAM IS COMPLETELY HARMLESS & THE OPEN SOURCE CAN BE FOUND ON GITHUB!")
     print("PRESS ENTER TO CONTINUE")
     eula = input("")
-    idiota(eula)
+    idiot(eula)
 
-    # cimző paraméterei
-    def cimzo():
+    # sendFrom's parameters
+    def sendFrom():
         try:
-            cimzo.cimzo_felhasznalonev = input("username: ")
-            cimzo.cimzo_jelszo = input("password: ")
+            sendFrom.sendFrom_username = input("username: ")
+            sendFrom.sendFrom_password = input("password: ")
             with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
                 smtp.ehlo()
                 smtp.starttls()
-                smtp.login(cimzo.cimzo_felhasznalonev, cimzo.cimzo_jelszo)
+                smtp.login(sendFrom.sendFrom_username, sendFrom.sendFrom_password)
         except smtplib.SMTPAuthenticationError as auth:
             clear()        
             print("ERROR: INVALID USERNAME / PASSWORD\nERROR: "+str(auth)+"\n")
-            cimzo()
+            sendFrom()
         except TypeError as typeerror:
             clear()
             print("ERROR: EMPTY USERNAME\nERROR: "+str(typeerror)+"\n")
-            cimzo()    
+            sendFrom()
+        except OSError as nErr:
+            clear()
+            print("ERROR: NETWORK IS UNREACHABLE.\nERROR: "+str(nErr))
+            sendFrom()
         clear()
         print("SUCCESSFUL LOGIN")
-    cimzo()
-    cimzo_felhasznalonev = cimzo.cimzo_felhasznalonev
-    cimzo_jelszo = cimzo.cimzo_jelszo
-    cimzo_nev = input("\n#MAIL FROM:\n")
+    sendFrom()
+    sendFrom_username = sendFrom.sendFrom_username
+    sendFrom_password = sendFrom.sendFrom_password
+    sendFrom_name = input("\n#MAIL FROM:\n")
 
-    # levél paraméterei
+    # the mail's parameters
     end = "¤"
     i = 0
-    level_targy = input("\n#LETTER SUBJECT\n")
-    level_tartalom = []
+    mail_subject = input("\n#LETTER SUBJECT\n")
+    mail_content = []
     def main():
-        main.level_tartalom = input("\n#LETTER CONTENT\n")
+        main.mail_content = input("\n#LETTER CONTENT\n")
         main.i = 2
-        main.kilépés = False
-        def ujsor():
-            if "¤" in main.level_tartalom:
-                main.level_tartalom = list(main.level_tartalom)
-                main.level_tartalom[-1] = "\n"
-                while main.kilépés != True:
-                    ujsor_var = input()
+        main.quit = False
+        def newLine():
+            if "¤" in main.mail_content:
+                main.mail_content = list(main.mail_content)
+                main.mail_content[-1] = "\n"
+                while main.quit != True:
+                    newLine_var = input()
                     main.i += 1
-                    main.level_tartalom.extend(ujsor_var)
-                    if "¤" in ujsor_var:
-                        ujsor()
+                    main.mail_content.extend(newLine_var)
+                    if "¤" in newLine_var:
+                        newLine()
                     else:
-                        main.kilépés = True
-        ujsor()
+                        main.quit = True
+        newLine()
     main()
-    level_tartalom = "".join(main.level_tartalom)
+    mail_content = "".join(main.mail_content)
     print()
 
-    def kuldes():
-        # címzett
-        cimzett_email = input("#MAIL TO\n")
-        while cimzett_email == "":
+    def sendMail():
+        # mailTo parameters
+        mailTo_email = input("#MAIL TO\n")
+        while mailTo_email == "":
             clear()
             print("SUCCESSFUL LOGIN!")
-            print("\n#MAIL FROM\n"+cimzo_nev)
-            print("\n#LETTER SUBJECT\n"+level_targy)
-            print("\n#LETTER CONTENT\n"+level_tartalom)
+            print("\n#MAIL FROM\n"+sendFrom_name)
+            print("\n#LETTER SUBJECT\n"+mail_subject)
+            print("\n#LETTER CONTENT\n"+mail_content)
             print("\nERROR: EMPTY ADDRESSEE, TRY AGAIN!")
-            cimzett_email = input("#MAIL TO\n")
+            mailTo_email = input("#MAIL TO\n")
 
-        # email objektívum létrehozása
+        # creating the email object
         email = EmailMessage()
-        # Név megadása a címző részéről
-        email["from"] = cimzo_nev
-        # Címezett megadása
-        email["to"] = cimzett_email
-        # Levél tárgyának megadása
-        email["subject"] = level_targy
-        email.set_content(level_tartalom)
+        email["from"] = sendFrom_name
+        email["to"] = mailTo_email
+        email["subject"] = mail_subject
+        email.set_content(mail_content)
 
-        # Hozzon létre STMP szervert
+        # create an smtp server
         with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
             smtp.ehlo()
-            # Biztonságosan rácsatlakozzon a szerverre
+            # securely connect to the server
             smtp.starttls()
-            # Belépés felhasználónév és jelszóval egy bábu gmailbe.
-            # https://myaccount.google.com/lesssecureapps <-- feltétlen be kell kapcsolni.
-            smtp.login(cimzo_felhasznalonev, cimzo_jelszo)
-            # Levél küldése
+            # log in to the account.
+            smtp.login(sendFrom_username, sendFrom_password)
+            # sending the mail
             try:
                 smtp.send_message(email)
             except smtplib.SMTPRecipientsRefused:
                 clear()
                 print("SUCCESSFUL LOGIN!")
-                print("\n#MAIL FROM\n"+cimzo_nev)
-                print("\n#LETTER SUBJECT\n"+level_targy)
-                print("\n#LETTER CONTENT\n"+level_tartalom)
+                print("\n#MAIL FROM\n"+sendFrom_name)
+                print("\n#LETTER SUBJECT\n"+mail_subject)
+                print("\n#LETTER CONTENT\n"+mail_content)
                 print("ERROR: INVALID ADDRESSSEE E-MAIL.")
-                kuldes()
+                sendMail()
             else:
                 print("\nTHE MESSAGE HAS BEEN SENT.")
-                utsolepes = input("PRESS ENTER TO EXIT\n")
-                idiota(utsolepes)
-    kuldes()
+                lastst = input("PRESS ENTER TO EXIT\n")
+                idiot(lastst)
+                sys.exit()
+    sendMail()
+
+
 def hu():
     clear()
     def idiota(var):
@@ -155,7 +158,11 @@ def hu():
         except TypeError as typeerror:
             clear()
             print("ERROR: ÜRES FELHASZNÁLÓNÉV\nERROR: "+str(typeerror)+"\n")
-            cimzo()    
+            cimzo()
+        except OSError as nErr:
+            clear()
+            print("ERROR: A HÁLÓZAT NEM ELÉRHETŐ\nERROR: "+str(nErr))
+            cimzo()   
         clear()
         print("SIKERES BEJELENTKEZÉS!")
     cimzo()
@@ -201,13 +208,9 @@ def hu():
             print("\nERROR: ÜRES CÍMZETT")
             cimzett_email = input("#CÍMZETT\n")
 
-        # email objektívum létrehozása
         email = EmailMessage()
-        # Név megadása a címző részéről
         email["from"] = cimzo_nev
-        # Címezett megadása
         email["to"] = cimzett_email
-        # Levél tárgyának megadása
         email["subject"] = level_targy
         email.set_content(level_tartalom)
 
@@ -216,8 +219,6 @@ def hu():
             smtp.ehlo()
             # Biztonságosan rácsatlakozzon a szerverre
             smtp.starttls()
-            # Belépés felhasználónév és jelszóval egy bábu gmailbe.
-            # https://myaccount.google.com/lesssecureapps <-- feltétlen be kell kapcsolni.
             smtp.login(cimzo_felhasznalonev, cimzo_jelszo)
             # Levél küldése
             try:
